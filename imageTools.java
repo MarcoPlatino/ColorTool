79d0ea7461fc795afbe4a005f877ae3f6de1da1d
Marco Platino <155002190+MarcoPlatino@users.noreply.github.com>
2025-06-05 08:18:26 -0400

    Update imageTools.java

--- imageTools.java
+++ imageTools.java
@@ -1,3 +1,44 @@
-public class imageTools {
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
+public class imageTools {
+	public static int a;
+	public static int r;
+	public static int b;
+	public static int g;
+	public static int p;
+	
+	public static void getPixelColor(String Path) {
+		BufferedImage img = null;
+		File f = null;
+
+		// read image
+		try {
+			f = new File(Path);
+			img = ImageIO.read(f);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+
+		// get image width and height
+		int width = img.getWidth();
+		int height = img.getHeight();
+
+		p = img.getRGB(0, 0);
+
+		// get alpha
+		a = (p >> 24) & 0xff;
+
+		// get red
+		r = (p >> 16) & 0xff;
+
+		// get green
+		g = (p >> 8) & 0xff;
+
+		// get blue
+		b = p & 0xff;
+
+	}
 }
