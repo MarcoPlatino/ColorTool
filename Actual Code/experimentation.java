b82b330b958fe6ecb5ae01a84e93bf1da8623fa0
Marco Platino <mistermarcop@hotmail.com>
2025-06-06 19:51:51 -0400

    short ahh comments

--- Actual Code/experimentation.java
+++ Actual Code/experimentation.java
@@ -1,3 +1,9 @@
+// Short Note:
+// This file is for just messing around to make sure that everything works
+// It is ok to change it or delete all the content and replace it with something else.
+// If it is not, just add a comment at the top to indicate that preserving the content of the file
+// Is important to somethant that you are working on.
+
 public class experimentation{
     public static void main(String[] args){
         System.out.println("Values Concerning BLLUE");
