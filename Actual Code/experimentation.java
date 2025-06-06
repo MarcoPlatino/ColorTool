b1ade797b97b0b143507cc7b54f62f9c8ce0c774
Marco Platino <mistermarcop@hotmail.com>
2025-06-06 16:32:44 -0400

    Everything has been updated in experimentation as demo. You may have to change the file path, but I don't know.........

--- Actual Code/experimentation.java
+++ Actual Code/experimentation.java
@@ -1,19 +1,19 @@
 public class experimentation{
     public static void main(String[] args){
         System.out.println("Values Concerning BLLUE");
-        imageTools.getPixelColorAverage("/workspaces/ColorTool/ExampleColors/BLUE.png");
+        imageTools.getPixelColorAverage("/media/marco/PRINTING/ColorTool/ExampleColors/BLUE.png");
         System.out.println(imageTools.averageR);
         System.out.println(imageTools.averageG);
         System.out.println(imageTools.averageB);
 
         System.out.println("Values Concerning RED");
-        imageTools.getPixelColorAverage("/workspaces/ColorTool/ExampleColors/RED.png");
+        imageTools.getPixelColorAverage("/media/marco/PRINTING/ColorTool/ExampleColors/GREEN.png");
         System.out.println(imageTools.averageR);
         System.out.println(imageTools.averageG);
         System.out.println(imageTools.averageB);
 
         System.out.println("Values Concerning YELLOW");
-        imageTools.getPixelColorAverage("/workspaces/ColorTool/ExampleColors/YEllOW.png");
+        imageTools.getPixelColorAverage("/media/marco/PRINTING/ColorTool/ExampleColors/YELLOW.png");
         System.out.println(imageTools.averageR);
         System.out.println(imageTools.averageG);
         System.out.println(imageTools.averageB);
