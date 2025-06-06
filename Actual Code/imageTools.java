6bc65134b8759a329b6d4b22c4a8fe136bd851ed
Marco Platino <155002190+MarcoPlatino@users.noreply.github.com>
2025-06-06 08:15:36 -0400

    Update imageTools.java
    Fixed stuff added funcitnos

--- Actual Code/imageTools.java
+++ Actual Code/imageTools.java
@@ -2,6 +2,9 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class imageTools {
 	public static int a;
@@ -10,7 +13,11 @@ public class imageTools {
 	public static int g;
 	public static int p;
 	
-	public static void getPixelColor(String Path) {
+	public static int averageR;
+	public static int averageG;
+	public static int averageB;
+	
+	public static void getPixelColor(String Path, int x, int y) {
 		BufferedImage img = null;
 		File f = null;
 
@@ -23,8 +30,8 @@ public class imageTools {
 		}
 
 		// get image width and height
-		int width = img.getWidth();
-		int height = img.getHeight();
+//		int width = img.getWidth();
+//		int height = img.getHeight();
 
 		p = img.getRGB(0, 0);
 
@@ -40,5 +47,48 @@ public class imageTools {
 		// get blue
 		b = p & 0xff;
 
+	}
+	
+	public static void getPixelColorAverage(String Path) {
+		BufferedImage img = null;
+		File f = null;
+		
+		try {
+			f = new File(Path);
+			img = ImageIO.read(f);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		
+		int width = img.getWidth();
+		int height = img.getHeight();
+		
+		int[] R;
+		int[] G;
+		int[] B;
+		
+		for(int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				p = img.getRGB(i, j);
+				// get alpha
+
+
+				// get red
+				R.add((p >> 16) & 0xff);
+
+				// get green
+				g = (p >> 8) & 0xff;
+
+				// get blue
+				b = p & 0xff;
+				
+			}
+		}
+		
+		
+		
+		
+		
+		
 	}
 }
