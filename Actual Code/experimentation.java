7c48e4bf92dae9e0bb4b729728138497b36c9a83
Marco Platino <155002190+MarcoPlatino@users.noreply.github.com>
2025-06-06 14:50:43 +0000

    Added more testing

--- Actual Code/experimentation.java
+++ Actual Code/experimentation.java
@@ -1,8 +1,23 @@
 public class experimentation{
     public static void main(String[] args){
+        System.out.println("Values Concerning BLLUE");
         imageTools.getPixelColorAverage("/workspaces/ColorTool/ExampleColors/BLUE.png");
         System.out.println(imageTools.averageR);
         System.out.println(imageTools.averageG);
         System.out.println(imageTools.averageB);
+
+        System.out.println("Values Concerning RED");
+        imageTools.getPixelColorAverage("/workspaces/ColorTool/ExampleColors/RED.png");
+        System.out.println(imageTools.averageR);
+        System.out.println(imageTools.averageG);
+        System.out.println(imageTools.averageB);
+
+        System.out.println("Values Concerning YELLOW");
+        imageTools.getPixelColorAverage("/workspaces/ColorTool/ExampleColors/YEllOW.png");
+        System.out.println(imageTools.averageR);
+        System.out.println(imageTools.averageG);
+        System.out.println(imageTools.averageB);
+    }
+    }
     }
 }
